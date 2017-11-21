d1501cd918d889052404363a3548eebdfd725f75
VaibhavBajaj <vbajajpersonal@gmail.com>
2017-11-20 17:23:21 -0800

    Added constants

---
+++ StudyBuddyBackend/constants.py
@@ -0,0 +1,12 @@
+UIUC_COURSES_API_URL = "http://courses.illinois.edu/cisapp/explorer/schedule/{0}.xml"
+
+DEPARTMENT_SEARCH_STRING = 'subject'
+DEPARTMENT_API = 'departments/{0}'
+
+COURSE_SEARCH_STRING = 'course'
+COURSE_API = 'courses/{0}/{1}'
+
+SECTION_SEARCH_STRING = 'section'
+SECTION_API = 'sections/{0}/{1}/{2}'
+
+SEMESTER = "fall"
